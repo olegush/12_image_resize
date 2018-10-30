b2b7cdce909866b57865dc97e3428fca61ee4ca9
olegush <43958767+olegush@users.noreply.github.com>
2018-10-30 11:43:01 +0300

    reduced Cyclomatic complexity

--- image_resize.py
+++ image_resize.py
@@ -14,36 +14,24 @@ def parser_args():
     return args
 
 
-def resize_image(img):
+def resize_image(filepath, width, height, scale, output):
     msgs = [
-        'Can not resize the image because too much size parameters.\n',
         'Please check aspect ratio, may be it does not match the original.\n',
-        'No parameters for resize.\n',
-        'The path is empty or does not exists. '
-        'File saved in the current directory.\n',
+        'The path is empty or not exists. Saved in the current directory.\n',
         'The result file was saved as '
     ]
     msg = ''
-    im = Image.open(img.filepath)
+    im = Image.open(filepath)
     original_width, original_height = im.size
-    width = img.width
-    height = img.height
-    scale = img.scale
-    filepath = img.filepath
-    output = img.output
     as_ratio = original_width / original_height
-    if not(width or height or scale):
-        return msgs[2]
-    if (width or height) and scale:
-        return msgs[0]
     if scale:
         newsize = round(original_width * scale), round(original_height * scale)
-    if width and height:
-        msg += msgs[1]
+    elif width and height:
+        msg += msgs[0]
         newsize = width, height
     elif width:
         newsize = int(width), round(int(width) / as_ratio)
-    elif height:
+    else:
         newsize = round(int(height) * as_ratio), int(height)
     filename = filepath.rsplit('.')[0]
     fileext = filepath.rsplit('.')[1]
@@ -56,12 +44,22 @@ def resize_image(img):
     if output is not None and os.path.isdir(str(output)):
         newfilepath = output + '/' + newfilepath
     else:
-        msg += msgs[3]
+        msg += msgs[1]
     imgnew.save(newfilepath)
-    msg += msgs[4] + newfilepath
+    msg += msgs[2] + newfilepath
     return msg
 
 
 if __name__ == '__main__':
     img = parser_args()
-    print(resize_image(img))
+    width = img.width
+    height = img.height
+    scale = img.scale
+    filepath = img.filepath
+    output = img.output
+    if not(width or height or scale):
+        print('No parameters for resize.\n')
+    elif (width or height) and scale:
+        print('Can not resize the image because too much size parameters.\n')
+    else:
+        print(resize_image(filepath, width, height, scale, output))
