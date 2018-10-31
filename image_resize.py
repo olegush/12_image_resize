dafc85181bcdfa5c8d7430d3fc5a6b1d692b9be2
olegush <43958767+olegush@users.noreply.github.com>
2018-10-31 11:48:10 +0300

    improved the code, fixed bugs, separated function

--- image_resize.py
+++ image_resize.py
@@ -9,26 +9,30 @@ def parser_args():
     parser.add_argument('--width', help='width for resize', type=int)
     parser.add_argument('--height', help='height for resize', type=int)
     parser.add_argument('--scale', help='scale for resize', type=float)
-    parser.add_argument('--output', help='path to result file')
+    parser.add_argument('--output', help='path to directory for result file')
     args = parser.parse_args()
     return args
 
 
+def resize_msgs(message_type):
+    msgs = {
+        'ratio': 'Aspect ratio does not match the original.\n',
+        'output': 'The path is not exists. Saved in the current dir.\n',
+        'final': 'The result file was saved in the output dir '
+    }
+    print(msgs[message_type])
+
+
 def resize_image(filepath, width, height, scale, output):
-    msgs = [
-        'Please check aspect ratio, may be it does not match the original.\n',
-        'The path is empty or not exists. Saved in the current directory.\n',
-        'The result file was saved as '
-    ]
-    msg = ''
     im = Image.open(filepath)
     original_width, original_height = im.size
-    as_ratio = original_width / original_height
+    as_ratio = round(original_width / original_height, 2)
     if scale:
         newsize = round(original_width * scale), round(original_height * scale)
     elif width and height:
-        msg += msgs[0]
         newsize = width, height
+        if as_ratio != round(width/height, 2):
+            resize_msgs('ratio')
     elif width:
         newsize = int(width), round(int(width) / as_ratio)
     else:
@@ -41,25 +45,27 @@ def resize_image(filepath, width, height, scale, output):
                                         str(newsize[1]),
                                         fileext
                                         )
-    if output is not None and os.path.isdir(str(output)):
-        newfilepath = output + '/' + newfilepath
+    if output and os.path.isdir(output):
+        newfilepath = os.path.join(output, newfilepath)
     else:
-        msg += msgs[1]
+        resize_msgs('output')
     imgnew.save(newfilepath)
-    msg += msgs[2] + newfilepath
-    return msg
+    resize_msgs('final')
+    return True
 
 
 if __name__ == '__main__':
-    img = parser_args()
-    width = img.width
-    height = img.height
-    scale = img.scale
-    filepath = img.filepath
-    output = img.output
+    args_img = parser_args()
+    width = args_img.width
+    height = args_img.height
+    scale = args_img.scale
+    filepath = args_img.filepath
+    output = args_img.output
+    if not os.path.isfile(filepath):
+        exit('No such file')
     if not(width or height or scale):
-        print('No parameters for resize.\n')
+        exit('No parameters for resize.\n')
     elif (width or height) and scale:
-        print('Can not resize the image because too much size parameters.\n')
+        exit('Can not resize the image because too much size parameters.\n')
     else:
-        print(resize_image(filepath, width, height, scale, output))
+        resize_image(filepath, width, height, scale, output)
